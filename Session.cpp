b4a33624d8f37973e937fbfa7cc5dfb14490d5a1
nyashka <sashakulashov@gmail.com>
2025-04-27 19:57:08 +0300

    nothing

--- Session.cpp
+++ Session.cpp
@@ -7,6 +7,25 @@ void Session::start() {
     initialization_read_and_continue();
 }
 
+void Session::initialization_read_and_continue() {
+    auto self(shared_from_this());
+    socket_.async_read_some(
+            boost::asio::buffer(data_, max_length),
+            [this, self](boost::system::error_code ec, std::size_t length) {
+                if (!ec) {
+                    data_[length] = '\0';
+                    std::string name(data_, length);
+                    std::cout << "his info:\n name: " << name << std::endl;
+                    user.update_name(name);
+                    do_write(length);
+                } else if (ec == boost::asio::error::eof || ec == boost::asio::error::connection_reset) {
+                    std::cout << "Client disconnected gracefully" << std::endl;
+                } else {
+                    std::cerr << "Read error: " << ec.message() << std::endl;
+                }
+            });
+}
+
 void Session::do_read() {
     auto self(shared_from_this());
     socket_.async_read_some(
@@ -34,25 +53,6 @@ void Session::do_write(std::size_t length) {
                     do_read(); // Продолжаем читать
                 } else {
                     std::cerr << "Write error: " << ec.message() << std::endl;
-                }
-            });
-}
-
-void Session::initialization_read_and_continue() {
-    auto self(shared_from_this());
-    socket_.async_read_some(
-            boost::asio::buffer(data_, max_length),
-            [this, self](boost::system::error_code ec, std::size_t length) {
-                if (!ec) {
-                    data_[length] = '\0';
-                    std::string name(data_, length);
-                    std::cout << "his info:\n name: " << name << std::endl;
-                    user.update_name(name);
-                    do_write(length);
-                } else if (ec == boost::asio::error::eof || ec == boost::asio::error::connection_reset) {
-                    std::cout << "Client disconnected gracefully" << std::endl;
-                } else {
-                    std::cerr << "Read error: " << ec.message() << std::endl;
                 }
             });
 }
